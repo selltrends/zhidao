03f01cf37e940a30cac61e23ed506301d5491780
Alex Wang <selltrends@gmail.com>
2015-11-18 14:47:13 +0800

    Trough yeoman init angular zhidao app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zhidaoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zhidaoApp
+ */
+angular.module('zhidaoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zhidaoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zhidaoApp
+ */
+angular.module('zhidaoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
