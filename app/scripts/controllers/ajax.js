d06dc6a412f115a9a840b6d6465badbfee5df6d6
Alex Wang <selltrends@gmail.com>
2015-11-19 15:42:35 +0800

    add ajax xml http method

--- app/scripts/controllers/ajax.js
+++ app/scripts/controllers/ajax.js
@@ -15,3 +15,21 @@ angular.module('zhidaoApp')
       });
     };
   });
+
+  angular.module('zhidaoApp')
+    .controller('AjaxMCtrl', function ($scope, $http) {
+      $scope.loadXmlData = function (){
+        $http.get('data/productData.xml').then(function (response){
+          $scope.products =[];
+          var productElems = angular.element(response.data.trim()).find('product');
+          for(var i=0; i < productElems.length; i++){
+            var product = productElems.eq(i);
+            $scope.products.push({
+              name: product.attr('name'),
+              category: product.attr('category'),
+              price: product.attr('price')
+            });
+          };
+        });
+      };
+    });
