4eb3716b6a63e26b77dd5c17755f4d4fc103fd45
Alex Wang <selltrends@gmail.com>
2015-11-23 15:15:07 +0800

    add form validation funciton

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,12 @@
  * Controller of the zhidaoApp
  */
 angular.module('zhidaoApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    $scope.phone = 138003800;
+    $scope.addUser = function (userDetails) {
+                    $scope.message = userDetails.name
+                        + " (" + userDetails.email + ") (" + userDetails.agreed + ")";
+}
+                $scope.message = "Ready";
+                $scope.matchPattern = new RegExp("^[\u4E00-\u9FA5][\da-zA-Z]{6}$");
   });
