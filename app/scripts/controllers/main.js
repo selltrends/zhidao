9d8df3bdb1bb46f6e12741f818b224ef22639268
Alex Wang <selltrends@gmail.com>
2015-11-24 10:19:57 +0800

    add selection form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,12 @@
 angular.module('zhidaoApp')
   .controller('MainCtrl', function ($scope) {
 
+    $scope.todos = [
+      {id:100,place:"Store",action:"Get groceries", complete:false},
+      {id:200,place:"Home",action:"Call plumber",complete:false},
+      {id:300,place:"Store",action:"Buy running shoes",complete:true}
+    ];
+
     $scope.addUser = function(userDetails){
       if (myForm.$valid) {
         $scope.message = userDetails.name + "(" + userDetails.email + ")(" + userDetails.agree + ")";
